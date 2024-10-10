a36e3c1f05051e38f87e67d02451bff38ddbcd38
shinis(home) <actionshin@gmail.com>
2024-10-10 22:31:32 +0900

    프로젝트 설정

---
+++ src/main/java/kr/co/cofile/hdcdmybatis/HdcdMybatisApplication.java
@@ -0,0 +1,13 @@
+package kr.co.cofile.hdcdmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdcdMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdcdMybatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/cofile/hdcdmybatis/HdcdMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.cofile.hdcdmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdcdMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
