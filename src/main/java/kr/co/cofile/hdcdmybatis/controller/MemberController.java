9be635fe021debad865bfc6ba16f4b57b74acea9
shinis(home) <actionshin@gmail.com>
2024-10-16 23:55:41 +0900

    입력값 검증

--- src/main/java/kr/co/cofile/hdcdmybatis/controller/MemberController.java
+++ src/main/java/kr/co/cofile/hdcdmybatis/controller/MemberController.java
@@ -8,6 +8,8 @@ import kr.co.cofile.hdcdmybatis.domain.Card;
 import kr.co.cofile.hdcdmybatis.domain.Member;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.slf4j.Slf4j;
@@ -145,5 +147,22 @@ public class MemberController {
         ResponseEntity<String> entity = new ResponseEntity<>("UPLOAD SUCCESS " + originalFilename, HttpStatus.OK);
 
         return entity;
+    }
+
+    @PostMapping(path = "", produces="text/plain;charset=utf-8")
+    public ResponseEntity<String> register(@Validated @RequestBody Member member, BindingResult result) {
+        log.info("register");
+
+        if(result.hasErrors()) {
+            return new ResponseEntity<>(result.toString(), HttpStatus.BAD_REQUEST);
+        }
+
+        log.info("member.getUserId() = " + member.getUserId());
+        log.info("member.getPassword() = " + member.getPassword());
+        log.info("member.getUserName() = " + member.getUserName());
+        log.info("member.getEmail() = " + member.getEmail());
+        log.info("member.getGender() = " + member.getGender());
+
+        return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
     }
 }
--- src/main/java/kr/co/cofile/hdcdmybatis/domain/Member.java
+++ src/main/java/kr/co/cofile/hdcdmybatis/domain/Member.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,9 +15,17 @@ import lombok.ToString;
 @ToString
 public class Member {
 
+    @NotBlank
     private String userId = "hongkd";
 
     private String password = "1234";
+
+    @NotBlank
+    @Size(max = 3)
+    private String userName;
+
+    private String email;
+    private String gender;
 
     private Address address;
 
