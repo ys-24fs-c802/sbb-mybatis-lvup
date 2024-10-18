58c460e8dedf4fea885b7decd68311ae99732708
shinis(ys) <actionshin@gmail.com>
2024-10-18 15:37:03 +0900

    입력갑 검증 연습

--- src/main/java/kr/co/cofile/hdcdmybatis/controller/MemberController.java
+++ src/main/java/kr/co/cofile/hdcdmybatis/controller/MemberController.java
@@ -3,15 +3,22 @@ package kr.co.cofile.hdcdmybatis.controller;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import kr.co.cofile.hdcdmybatis.domain.Address;
 import kr.co.cofile.hdcdmybatis.domain.Card;
 import kr.co.cofile.hdcdmybatis.domain.Member;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
@@ -119,6 +126,20 @@ public class MemberController {
 
         log.info("member.getDateOfBirth() = " + member.getDateOfBirth());
 
+        return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
+    }
+    
+    @PostMapping(path = "", produces="text/plain;charset=utf-8")
+    public ResponseEntity<String> register(@Validated @RequestBody Member member, BindingResult result) {
+        log.info("register");
+        if(result.hasErrors()) {
+            return new ResponseEntity<>(result.toString(), HttpStatus.BAD_REQUEST);
+        }
+        log.info("member.getUserId() = " + member.getUserId());
+        log.info("member.getPassword() = " + member.getPassword());
+        log.info("member.getUserName() = " + member.getUserName());
+        log.info("member.getEmail() = " + member.getEmail());
+        log.info("member.getGender() = " + member.getGender());
         return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
     }
 
